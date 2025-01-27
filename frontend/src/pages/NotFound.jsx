0f4c50f42fc10bb24fd023297d8995ca054b0a32
Edan A <edan.aubin@outlook.com>
2025-01-27 14:48:00 -0500

    Ajouts texte

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,8 @@
 function NotFound() {
-    return <div>Not Found</div>
+    return <div>     
+        <h1>404 Not Found</h1>
+        <p>La page que vous cherchez n'existe pas!</p>
+        </div>
 }
 
 export default NotFound
